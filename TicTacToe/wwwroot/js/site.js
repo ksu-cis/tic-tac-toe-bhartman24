dcad275f6788eb99d5c5918706bf8bf71e5071bf
bhartman24 <bhartman24@ksu.edu>
2019-11-11 15:15:29 -0600

    TicTacToe complete

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,60 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var turn = "x";
+
+function setTurn() {
+    var turnElement = document.getElementById("turn");
+    turnElement.innerText = "It is player " + turn + " 's turn";
+}
+
+function setWinner(winner) {
+    var turnElement = document.getElementById("turn");
+    turnElement.innerText = "Player " + winner + " wins!";
+}
+
+function checkForWin() {
+    var cells = document.getElementsByClassName("square");
+
+    //check horizontal cells
+    for (var i = 0; i < 9; i += 3) {
+        if (cells[i].innerText && cells[0 + i].innerText === c[1 + i].innerText && cells[1 + i].innerText === cells[2 + i].innerText) {
+            setWinner(cells[i]);
+            return true;
+        }
+    }
+
+    //check vertical cells
+    for (var j = 0; j < 9; j += 3) {
+        if (cells[j].innerText && cells[0 + j].innerText === cells[3 + j].innerText && cells[3 + j].innerText === cells[6 + j].innerText) {
+            setWinner(cells[j].innerText);
+            return true;
+        }
+    }
+
+    //check diagonal
+
+    return false;
+}
+
+function onClick(event) {
+    event.preventDefault();
+    if (!event.target.innerText) {
+        event.target.innerText = turn;
+        if (turn === "x") {
+            turn = "o";
+        }
+        else {
+            turn = "x";
+        }
+        if (!checkForWin()) setTurn();
+    }
+}
+
+var cells = document.getElementsByClassName("square");
+for (i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", onClick);
+}
+
+setTurn();
